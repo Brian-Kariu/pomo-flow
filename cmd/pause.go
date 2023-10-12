7ef4e7b8a399ae3457963a42bb0bc78bccd923c9
Brian Kariu <waiganjo.brian@gmail.com>
2023-10-12 21:38:53 +0300

    feat: Created basic CLI scaffold
    - Created initial state of the cli
    - Added viper and cobra go packages

---
+++ cmd/pause.go
@@ -0,0 +1,52 @@
+/*
+Copyright © 2023 Brian Kariu <github.com/Brian-Kariu/>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// pauseCmd represents the pause command
+var pauseCmd = &cobra.Command{
+	Use:   "pause",
+	Short: "Pause the current task timer",
+	Long: `Pauses the timer of the current task until the user restarts it.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Timer has been paused!")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(pauseCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// pauseCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// pauseCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
